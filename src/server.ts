1c9d2ccc95f4af361fb2294f5715960ac504e815
Evgeniy Logvinov <evgeniy.logvinov.k@gmail.com>
2021-09-15 12:49:26 +0300

    working example

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import app from './app';
+import express from 'express';
 
-const PORT = process.env.PORT || '3000';
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
