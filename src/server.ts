18d674a035d25c1ac64a2dc9252b8ef49f0eb945
Evgeniy Logvinov <evgeniy.logvinov.k@gmail.com>
2021-09-16 10:28:02 +0300

    test console.log

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from './app';
 
 const PORT = process.env.PORT || 3000;
-
+console.log('Test console. log string');
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
